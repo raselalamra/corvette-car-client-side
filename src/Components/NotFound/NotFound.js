2f9857fa39b95846e027bc2adfe950f790534a50
raselalamjubayed <raselalam838@gmil.com>
2021-11-12 02:17:10 +0600

    code add

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -2,14 +2,14 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import oops from '../../images/oops.png';
 const goBack = () => window.history.back();
 
 const NotFound = () => {
     return (
         <div className="d-flex align-items-center justify-content-center text-center" style={{height:"100vh"}}>
         <div style={{border:'1px solid gray', padding:'30px'}}>
-             <h1 className="display-1">404</h1>
-             <p className="lead">Page Not Found</p>
+             <img src={oops} style={{width: '50%'}} alt="" />
              <Button variant='info'  onClick={goBack} className="brand-btn"  to="/"><FontAwesomeIcon className="mr-2" icon={faArrowLeft}/>Back To Home</Button>
         </div>
      </div>
